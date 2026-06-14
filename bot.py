1d09eea1c901ce86fe7e33d67a5454db020202d5
TtiGalki <58266486+TtiGalki@users.noreply.github.com>
2026-06-14 17:15:28 +0300

    Refactor error messages and remove health checkco

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import logging
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from aiohttp import web
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -15,19 +14,15 @@ async def privet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hydra Dominatus")
 
 
-async def health(request):
-    return web.Response(text="OK")
-
-
 def main() -> None:
     token = os.getenv("BOT_TOKEN")
     webhook_url = os.getenv("WEBHOOK_URL")
     port = int(os.getenv("PORT", 8080))
 
     if not token:
-        raise ValueError("Переменная окружения BOT_TOKEN не задана!")
+        raise ValueError("BOT_TOKEN не задан!")
     if not webhook_url:
-        raise ValueError("Переменная окружения WEBHOOK_URL не задана!")
+        raise ValueError("WEBHOOK_URL не задан!")
 
     app = ApplicationBuilder().token(token).build()
     app.add_handler(CommandHandler("hi", privet))
@@ -36,6 +31,7 @@ def main() -> None:
     app.run_webhook(
         listen="0.0.0.0",
         port=port,
+        secret_token=None,
         webhook_url=f"{webhook_url}/webhook",
         url_path="/webhook",
     )
